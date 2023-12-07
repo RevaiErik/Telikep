288b3b1746f6ec77e62725c1ccbb2bc6fd026b5a
RevaiErik <revaierik100@gmail.com>
2023-12-07 16:56:24 +0100

    Add project files.

---
+++ Telikep/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Telikep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bearnose_btn = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bearnose_btn
+            // 
+            this.bearnose_btn.BackColor = System.Drawing.Color.Transparent;
+            this.bearnose_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bearnose_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bearnose_btn.ForeColor = System.Drawing.Color.Transparent;
+            this.bearnose_btn.Location = new System.Drawing.Point(452, 489);
+            this.bearnose_btn.Name = "bearnose_btn";
+            this.bearnose_btn.Size = new System.Drawing.Size(28, 21);
+            this.bearnose_btn.TabIndex = 0;
+            this.bearnose_btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.bearnose_btn.UseVisualStyleBackColor = false;
+            this.bearnose_btn.Click += new System.EventHandler(this.bearnose_btn_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, -5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1012, 997);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1008, 985);
+            this.Controls.Add(this.bearnose_btn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Téli kép";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bearnose_btn;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Telikep/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Telikep
+{
+    public partial class Form1 : Form
+    {
+        public class Hopelyhek
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Mennyivel_Valtozik_X { get; set; }
+            public int Mennyivel_Valtozik_Y { get; set; }
+        }
+
+
+        private List<Hopelyhek> hopelyhek = new List<Hopelyhek>();
+        private Image hopehelykepek = Image.FromFile(@"..\..\src\snowflakes.png");
+        private int hopehelyszamokmaximuma = 40;
+        private SoundPlayer zenelejátszó = new SoundPlayer(@"..\..\src\Michael_Bublé_It's_Beginning_To_Look_A_Lot_Like_Christmas.wav");
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Start();
+        }
+
+        private void bearnose_btn_Click(object sender, EventArgs e)
+        {
+           
+            zenelejátszó.Play();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Random vel = new Random();
+
+            if (hopelyhek.Count < hopehelyszamokmaximuma)
+            {
+                Hopelyhek uj_hopehely = new Hopelyhek();
+                uj_hopehely.X =  vel.Next(0, pictureBox1.Width);
+                uj_hopehely.Y = -hopehelykepek.Height;
+                uj_hopehely.Mennyivel_Valtozik_X = vel.Next(-5, 5);
+                uj_hopehely.Mennyivel_Valtozik_Y = vel.Next(1, 10);
+                hopelyhek.Add(uj_hopehely);
+            }
+
+            foreach (var item in hopelyhek)
+            {
+                item.X += item.Mennyivel_Valtozik_X;
+                item.Y += item.Mennyivel_Valtozik_Y;
+
+                if (item.X < -hopehelykepek.Width || item.X > pictureBox1.Width ||
+                    item.Y > pictureBox1.Height)
+                {
+                    item.X = vel.Next(0, pictureBox1.Width);
+                    item.Y = -hopehelykepek.Height;
+                    item.Mennyivel_Valtozik_X = vel.Next(-5, 5);
+                    item.Mennyivel_Valtozik_Y = vel.Next(1, 10);
+                }
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var item in hopelyhek)
+            {
+                e.Graphics.DrawImage(hopehelykepek, item.X, item.Y);
+            }
+        }
+    }
+}
